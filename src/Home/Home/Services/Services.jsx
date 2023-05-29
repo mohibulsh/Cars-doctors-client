115108ccd5ac9ad5d3ef0a1f4105365efff4167a
akalaminamin <alamin@hypescout.co>
2023-05-29 14:48:17 +0600

    add the toggle btn

--- src/Home/Home/Services/Services.jsx
+++ src/Home/Home/Services/Services.jsx
@@ -3,11 +3,12 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services ,setServices] =useState([])
+    const [ase ,setAse]=useState(true)
     useEffect(()=>{
-        fetch('https://car-doctors-server-two.vercel.app/services')
+        fetch(`http://localhost:5000/services?sort=${ase?'ase':'dse'}`)
         .then(res => res.json())
         .then (data => setServices(data))
-    },[])
+    },[ase])
     return (
         <div className='my-20'>
             <div className='text-center space-y-3'>
@@ -15,6 +16,8 @@ const Services = () => {
                  <h1 className='text-5xl font-bold'>Our Service Area</h1>
                  <p>the majority have suffered alteration in some form, by injected humour,
                      or randomised <br /> words which don't look even slightly believable. </p>
+
+                     <button className='btn btn-primary' onClick={()=>setAse(!ase)}>{ase?'Price High to Low':'Price Low to High'}</button>
             </div>
           <div className='grid mt-12 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8'>
           {
